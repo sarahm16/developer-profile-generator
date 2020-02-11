07330a5f78bc2a38f659de6d326a670543f82098
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-02-11 12:01:03 -0800

    exported generateHTML function, called function in index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const inquirer = require('inquirer');
+const variables = require('./generateHTML');
+const fs = require('fs');
+console.log(typeof variables.generateHTML);
 
 inquirer.prompt(
     [
         {
-            type: "input",
-            message: "What is your favorite color?",
-            name: "fav-color"
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username'
+
+        },
+        {
+            type: 'input',
+            message: 'What is your favorite color?',
+            name: 'color'
         }
     ]
-).then(function(responses) {
-    
+).then(function(data) {
+    //console.log(data.color);
+    variables.generateHTML(data);
+    //console.log(data.color);
+}).then(function(data) {
+
 })
